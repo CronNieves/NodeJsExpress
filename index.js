11c5cc70f7195a515578ec2b8510c386452cbae3
CronNieves <santiago.nieves@fing.edu.uy>
2019-02-11 14:25:11 -0300

    Express + Hsb

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const hbs = require('hbs');
+require('./hbs/helpers');
 const app = express();
+
 
 //Middlewares
 //El next hace que continuo el el codigo luego del use
@@ -8,22 +11,43 @@ const app = express();
     console.log('request url: ' + req.url);
     next();
 });*/
+
 //Middlewares para loguear los request
 app.use(morgan('combined'));
 
 
+//Archivos
+app.use(express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
+//Hbs
+hbs.registerPartials(__dirname + '/views/includes');
+app.set('view engine','hbs');
+
+//Rutas
 app.get('/', (req, res) => { 
-    res.end('Hellow World');
+    
+    res.render('home',{
+        nombre: 'Santiago'
+    });
 });
 
-app.get('/obtenerHola', (req, res) => { 
-    res.end('Hola');
+
+app.get('/about', (req, res) => { 
+    
+    res.render('about',{
+        nombre: 'Santiago'
+    });
 });
 
 app.get('*', (req, res) => { 
     res.end('Archivo no encontrado');
 });
 
+
+//Servidor escuchando
 app.listen(3000, function() {
     console.log('Servidor Online');
 });
